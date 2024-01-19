5ea623659bfe00c5084df56e92e810252fe026bd
David Samson <david.andrew.engineer@gmail.com>
2024-01-19 11:50:13 -0500

    moved symbol table into separate file. created blank file for vmwriter

--- projects/11/CompilationEngine.py
+++ projects/11/CompilationEngine.py
@@ -1,7 +1,6 @@
-from pathlib import Path
 from utils import Ref
 from JackTokenizer import Token
-from dataclasses import dataclass
+from SymbolTable import SymbolTable
 
 ##
 import pdb
@@ -11,35 +10,6 @@ import pdb
 XML = None
 
 
-@dataclass
-class Symbol:
-    # name: str # key in the symbol table
-    type: str
-    kind: str
-    index: int
-
-
-class SymbolTable(dict[str, Symbol]):
-    def insert(self, name: str, type: str, kind: str) -> None:
-        self[name] = Symbol(type, kind, self.var_count(kind))
-
-    def var_count(self, kind: str) -> int:
-        return len([symbol for symbol in self.values() if symbol.kind == kind])
-
-    def __str__(self) -> str:
-        """nice table printout"""
-        name_width = max(*(len(name) for name in self.keys()), len("name"))
-        type_width = max(*(len(symbol.type) for symbol in self.values()), len("type"))
-        kind_width = max(*(len(symbol.kind) for symbol in self.values()), len("kind"))
-        index_width = max(*(len(str(symbol.index)) for symbol in self.values()), len("#"))
-
-        header = f"{'name':<{name_width}} │ {'type':<{type_width}} │ {'kind':<{kind_width}} │ {'#':<{index_width}}"
-        lines = [header]
-        lines.append(f"{'─' * name_width}─┼─{'─' * type_width}─┼─{'─' * kind_width}─┼─{'─' * index_width}─")
-        for name, symbol in self.items():
-            lines.append(f"{name:<{name_width}} │ {symbol.type:<{type_width}} │ {
-                         symbol.kind:<{kind_width}} │ {symbol.index:<{index_width}}")
-        return "\n".join(lines)
 
 
 def compile(tokens: list[Token]) -> str:
@@ -263,6 +233,8 @@ def compile_subroutine_body(tokens_ref: Ref[list[Token]], class_symbols: SymbolT
     while compile_var_dec(tokens_ref, subroutine_symbols):
         ...
 
+    print(subroutine_symbols)
+    print(class_symbols)
     pdb.set_trace()
     # statements
     if not compile_statements(tokens_ref, branch):
---
+++ projects/11/SymbolTable.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass
+
+##
+import pdb
+
+@dataclass
+class Symbol:
+    # name: str # key in the symbol table
+    type: str
+    kind: str
+    index: int
+
+
+class SymbolTable(dict[str, Symbol]):
+    def insert(self, name: str, type: str, kind: str) -> None:
+        self[name] = Symbol(type, kind, self.var_count(kind))
+
+    def var_count(self, kind: str) -> int:
+        return len([symbol for symbol in self.values() if symbol.kind == kind])
+
+    def __str__(self) -> str:
+        """nice table printout"""
+        name_width = max([*(len(name) for name in self.keys()), len("name")])
+        type_width = max([*(len(symbol.type) for symbol in self.values()), len("type")])
+        kind_width = max([*(len(symbol.kind) for symbol in self.values()), len("kind")])
+        index_width = max([*(len(str(symbol.index)) for symbol in self.values()), len("#")])
+
+        header = f"{'name':<{name_width}} │ {'type':<{type_width}} │ {'kind':<{kind_width}} │ {'#':<{index_width}}"
+        lines = [header]
+        lines.append(f"{'─' * name_width}─┼─{'─' * type_width}─┼─{'─' * kind_width}─┼─{'─' * index_width}─")
+        for name, symbol in self.items():
+            lines.append(f"{name:<{name_width}} │ {symbol.type:<{type_width}} │ {
+                         symbol.kind:<{kind_width}} │ {symbol.index:<{index_width}}")
+        return "\n".join(lines)
